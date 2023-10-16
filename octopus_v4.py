1738a79579a3218130e5a45ad7b5fe36aaf43d8c
takado <kornelgut@gmail.com>
2023-10-17 00:27:28 +0200

    income statistics

--- octopus_v4.py
+++ octopus_v4.py
@@ -5,19 +5,29 @@ from sc2.ids.unit_typeid import UnitTypeId
 from sc2.position import Point2
 from sc2.unit import Unit
 
+from economy.income_statistics import IncomeStatistics
 from strategy.gate import Gate
 from strategy.strategy import Strategy
+import matplotlib.pyplot as plt
 
 
 class OctopusV4(BotAI):
     def __init__(self):
         self.strategy: Strategy = None
+        self.income_stats: IncomeStatistics = None
 
     async def on_start(self):
         self.strategy = Gate(self)
+        self.income_stats = IncomeStatistics(self)
 
     async def on_step(self, iteration: int):
         await self.strategy.execute()
+        self.income_stats.log()
+        if self.time > 420:
+            income_eval = self.income_stats.eval_income_per_worker()
+            print(income_eval)
+            self.income_stats.write_dict_to_file('eval_data.json', income_eval)
+            raise ValueError
 
     async def build(
         self,
