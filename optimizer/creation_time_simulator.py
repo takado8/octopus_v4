7da7c29675aed36d9c8d5d575aa83616c5b27abc
takado <kornelgut@gmail.com>
2023-10-17 01:03:15 +0200

    Create creation_time_simulator.py

---
+++ optimizer/creation_time_simulator.py
@@ -0,0 +1,8 @@
+
+class CreationTimeSimulator:
+    def __init__(self, ai):
+        self.ai = ai
+
+    def get_unit_creation_time(self, unit_type_id):
+        unit_type_data = self.ai._game_data.units[unit_type_id.value]
+        return int(round(unit_type_data.cost.time / 22.4))
